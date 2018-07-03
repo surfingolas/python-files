1d217aa433137bca3fc16f2b0736826c4d42ce48
PIP <surfingolas@gmail.com>
2018-07-03 08:02:04 -0700

    updated with infinite loop

--- trying_nums.py
+++ trying_nums.py
@@ -11,9 +11,15 @@ while (am2_dims >= 30.01) and (am2_dims <= 82.73):
     am2.append(round(am2_dims, 2))
     am2_dims = am2_dims + 0.01
 
-which_fin = float(input("enter your volume_"))
-print(which_fin)
-if which_fin in am1:
-    print("am1")
-else:
-    print("am2")
+while True:
+    try:
+        which_fin = float(input("enter your volume_"))
+    except ValueError:
+        break
+    print(which_fin)
+    if which_fin in am1:
+        print("am1")
+    elif which_fin in am2:
+        print("am2")
+    else:
+        print("no matching fins")
